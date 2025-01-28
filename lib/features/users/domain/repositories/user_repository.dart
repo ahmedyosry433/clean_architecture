2df6008ec336545f01edc352eea600f4e38d121c
Ahmed Elesawy <56431746+ahmedyosry433@users.noreply.github.com>
2025-01-28 13:04:14 +0200

    user repository

---
+++ lib/features/users/domain/repositories/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:clean_architecture/core/errors/failure.dart';
+import 'package:clean_architecture/features/users/domain/entities/user_entities.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UserRepository {
+  Future<Either<Failure, UserEntity>> getUser();
+}
